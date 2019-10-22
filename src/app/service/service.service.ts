820e80319e1dc7eac1bead98cef1fdd7e64dc02c
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-10-21 21:04:47 -0300

    first commit

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -7,7 +7,7 @@ import { Pessoa } from '../model/pessoa';
 })
 export class ServiceService {
 
-  url = 'https://cienciarede.herokuapp.com'
+  url = 'https://server-redesocial.herokuapp.com/redesocial'
 
   constructor(private http: HttpClient) { }
 
