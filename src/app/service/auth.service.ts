2fa4707cd39d1dbb04e3178962e61b89b4820f69
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-10-28 18:16:54 -0300

    first commit

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-    url = 'https://server-redesocial.herokuapp.com/redesocial';
+    url = 'https://redecientista.herokuapp.com';
 
     constructor(private httpClient: HttpClient, private router: Router) { }
 
--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -7,7 +7,7 @@ import { Pessoa } from '../model/pessoa';
 })
 export class ServiceService {
 
-  url = 'https://server-redesocial.herokuapp.com/redesocial'
+  url = 'https://redecientista.herokuapp.com'
 
   constructor(private http: HttpClient) { }
 
