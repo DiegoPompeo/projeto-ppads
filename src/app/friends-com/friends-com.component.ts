4cac2fa90414cb2cc740427cf43cd6d4ba83f687
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-24 14:36:54 -0300

    teste

--- src/app/friends-com/friends-com.component.ts
+++ src/app/friends-com/friends-com.component.ts
@@ -1,32 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Pessoa } from '../model/pessoa';
 
 @Component({
   selector: 'app-friends-com',
   templateUrl: './friends-com.component.html',
   styleUrls: ['./friends-com.component.css']
 })
-export class FriendsComComponent implements OnInit {
-
-  amigosEmComum = [];
+export class FriendsComComponent implements OnInit {  
+  
+  amigosEmComum: Pessoa[] = new Array<Pessoa>();
 
   @Input("listaAmigosDetails")
-  listaAmigosDetails = [];
+  listaAmigosDetails: Pessoa[] = new Array<Pessoa>();
 
   @Input("listaAmigos")
-  listaAmigos = [];
+  listaAmigos: Pessoa[] = new Array<Pessoa>();
 
   constructor() { }
 
   ngOnInit() {
-    this.listaAmigosDetails.forEach(x => {
-      this.listaAmigos.forEach(
-        y => {
-          if(x.email = y.email){
-            this.amigosEmComum.push(x);
-          }
-        }
-      );
-    });
+    for (let i = 0; i < this.listaAmigos.length; i++) {
+      for (let j = 0; j < this.listaAmigosDetails.length; j++) {
+        if (this.listaAmigos[i].email == this.listaAmigosDetails[j].email) {
+          this.amigosEmComum.push(this.listaAmigos[i]);
+        }        
+      }      
+    }
   }
 
 }
